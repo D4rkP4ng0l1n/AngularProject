fb50dcc65b09feeadc5bb1558de4dd8e38a19289
Clément Rieuneau <clem.rieuneau@gmail.com>
2026-04-17 14:36:20 +0200

    frontend : Accessibilité

--- frontend/src/app/components/product-list/product-list.ts
+++ frontend/src/app/components/product-list/product-list.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { ClsProduct } from '../../Models/cls-product-card';
+import { CartService } from '../../services/cart.service';
 import { ProductService } from '../../services/product.service';
 import { ProductCard } from '../product-card/product-card';
 
@@ -9,18 +12,100 @@ import { ProductCard } from '../product-card/product-card';
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList implements OnInit {
+export class ProductList implements OnInit, OnDestroy {
   severalProduct !: ClsProduct[];
+  goldenFreddyUnlocked: boolean = false;
+  
+  private destroy$ = new Subject<void>();
 
-  constructor(private productService: ProductService, private productChangeDetector: ChangeDetectorRef) { }
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private productChangeDetector: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
+    this.loadProducts();
+    
+    // Écouter les changements du panier
+    this.cartService.cartChanged$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.checkGoldenFreddyCondition();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  loadProducts(): void {
     this.productService.getProduct().subscribe(products => {
       this.severalProduct = products;
+      this.checkGoldenFreddyCondition();
       console.log(this.severalProduct);
       this.productChangeDetector.detectChanges();
     });
   }
+
+  checkGoldenFreddyCondition(): void {
+    this.cartService.getCart().subscribe(cartItems => {
+      const cartMap = this.createCartMap(cartItems);
+      
+      const hasFreddyCondition = 
+        cartMap['Freddy Fazbear'] === 1 &&
+        cartMap['Bonnie'] === 9 &&
+        cartMap['Chica'] === 8 &&
+        cartMap['Foxy'] === 7;
+
+      console.log('Cart Map:', cartMap);
+      console.log('Golden Freddy condition:', hasFreddyCondition);
+      
+      this.goldenFreddyUnlocked = hasFreddyCondition;
+      
+      if (hasFreddyCondition && !this.isGoldenFreddyInList()) {
+        this.addGoldenFreddy();
+      } else if (!hasFreddyCondition && this.isGoldenFreddyInList()) {
+        this.removeGoldenFreddy();
+      }
+      
+      this.productChangeDetector.detectChanges();
+    });
+  }
+
+  private createCartMap(cartItems: any[]): { [key: string]: number } {
+    const map: { [key: string]: number } = {};
+    cartItems.forEach(item => {
+      map[item.product_id.name] = item.quantity;
+    });
+    return map;
+  }
+
+  isGoldenFreddyInList(): boolean {
+    return this.severalProduct?.some(p => p._id === '1987') ?? false;
+  }
+
+  getGoldenFreddy(): ClsProduct | undefined {
+    return this.severalProduct?.find(p => p._id === '1987');
+  }
+
+  getNormalProducts(): ClsProduct[] {
+    return this.severalProduct?.filter(p => p._id !== '1987') ?? [];
+  }
+
+  private addGoldenFreddy(): void {
+    if (this.severalProduct && !this.isGoldenFreddyInList()) {
+      const goldenFreddy = new ClsProduct('1987', 'Golden Freddy', '/img/GoldenFreddy.webp');
+      this.severalProduct = [goldenFreddy, ...this.severalProduct];
+      this.productChangeDetector.detectChanges();
+    }
+  }
+
+  private removeGoldenFreddy(): void {
+    if (this.severalProduct) {
+      this.severalProduct = this.severalProduct.filter(p => p._id !== '1987');
+      this.productChangeDetector.detectChanges();
+    }
+  }
 }
-
-
--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { ClsCartItem } from "../Models/cls-cart-item";
 
 @Injectable({
@@ -10,6 +10,10 @@ export class CartService {
 
     private apiUrl = 'http://localhost:3000/cart';
     private postUrl = this.apiUrl + '/add';
+    
+    // Subject pour notifier les changements du panier
+    private cartChangedSubject = new Subject<void>();
+    public cartChanged$ = this.cartChangedSubject.asObservable();
 
     constructor(private http: HttpClient) { }
 
@@ -18,17 +22,50 @@ export class CartService {
     }
 
     addToCart(product_id: string, quantity: number): Observable<any> {
-        return this.http.post(this.postUrl, {
-            product_id,
-            quantity
+        return new Observable(observer => {
+            this.http.post(this.postUrl, {
+                product_id,
+                quantity
+            }).subscribe({
+                next: (res) => {
+                    this.cartChangedSubject.next();
+                    observer.next(res);
+                    observer.complete();
+                },
+                error: (err) => {
+                    observer.error(err);
+                }
+            });
         });
     }
 
     removeFromCart(cartItemId: string): Observable<any> {
-        return this.http.delete(this.apiUrl + '/' + cartItemId);
+        return new Observable(observer => {
+            this.http.delete(this.apiUrl + '/' + cartItemId).subscribe({
+                next: (res) => {
+                    this.cartChangedSubject.next();
+                    observer.next(res);
+                    observer.complete();
+                },
+                error: (err) => {
+                    observer.error(err);
+                }
+            });
+        });
     }
 
     clearCart(): Observable<any> {
-        return this.http.delete(this.apiUrl);
+        return new Observable(observer => {
+            this.http.delete(this.apiUrl).subscribe({
+                next: (res) => {
+                    this.cartChangedSubject.next();
+                    observer.next(res);
+                    observer.complete();
+                },
+                error: (err) => {
+                    observer.error(err);
+                }
+            });
+        });
     }
 }
