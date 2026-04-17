16037d194edfb9816eeaf57d64f47a6aba9b7336
Clément Rieuneau <clem.rieuneau@gmail.com>
2026-04-17 13:50:06 +0200

    frontend : Update css

--- frontend/src/app/components/shop-cart/shop-cart.ts
+++ frontend/src/app/components/shop-cart/shop-cart.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ClsCartItem } from '../../Models/cls-cart-item';
 import { CartService } from '../../services/cart.service';
-import { CartItem } from '../cart-item/cart-item';
 
 @Component({
   selector: 'app-shop-cart',
-  imports: [CartItem],
   templateUrl: './shop-cart.html',
   styleUrl: './shop-cart.scss',
 })
@@ -15,7 +13,11 @@ export class ShopCart implements OnInit {
   constructor(private cartService: CartService, private cartItemChangeDetector: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.cartService.getCart().subscribe(cartItems =>{
+    this.loadCart();
+  }
+
+  loadCart(): void {
+    this.cartService.getCart().subscribe(cartItems => {
       this.serveralCartItem = cartItems;
       this.cartItemChangeDetector.detectChanges();
     })
